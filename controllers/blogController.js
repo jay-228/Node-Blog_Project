035eb3acff67732c864aead8c135466be0c93b87
jay-228 <jay.mistry2208@gmail.com>
2024-12-12 18:12:12 +0530

    done

---
+++ controllers/blogController.js
@@ -0,0 +1,38 @@
+const BlogPost = require('../models/blog');
+
+exports.getPosts = async (req, res) => {
+  try {
+    const posts = await BlogPost.find();
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.createPost = async (req, res) => {
+  try {
+    const newPost = new BlogPost(req.body);
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.updatePost = async (req, res) => {
+  try {
+    const updatedPost = await BlogPost.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.deletePost = async (req, res) => {
+  try {
+    await BlogPost.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: 'Post deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
